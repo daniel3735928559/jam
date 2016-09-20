094fb57a46ad96959e8afb1d1fb6ad5c6b866324
DR <daniel3735928559@gmail.com>
2016-09-20 23:44:30 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# jam
+
+Mango communications node
---
+++ make.sh
@@ -0,0 +1,1 @@
+gcc `pkg-config --libs glib-2.0` -I /usr/include/libpurple/ -I /usr/include/glib-2.0/ -I /usr/lib/glib-2.0/include/ -lpurple hello.c -o hello

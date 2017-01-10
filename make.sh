f679540e3a66003a859b2e0cedcebd6f9beb696e
DR <daniel3735928559@gmail.com>
2017-01-10 22:46:44 +0000

    make.sh now allows specification of mango path

--- make.sh
+++ make.sh
@@ -1,1 +1,1 @@
-gcc -g `pkg-config --libs glib-2.0` -lzmq -L ../mango/libmango/c/ -I ../mango/libmango/c -I /usr/include/libpurple/ -I /usr/include/glib-2.0/ -I /usr/lib/glib-2.0/include/ -lmango -lyaml -lpurple jam.c -o jam
+gcc -g `pkg-config --libs glib-2.0` -lzmq -L "$MANGO"/libmango/c/ -I "$MANGO"/libmango/c -I /usr/include/libpurple/ -I /usr/include/glib-2.0/ -I /usr/lib/glib-2.0/include/ -lmango -lyaml -lpurple jam.c -o jam

a37b6559e76de717a3b21b980e084bab9dfb5a8c
DR <daniel3735928559@gmail.com>
2017-10-16 02:13:40 +0000

    fuzzy matching

--- dbus/jam.py
+++ dbus/jam.py
@@ -8,7 +8,7 @@ from fuzzywuzzy import process
 class jam(m_node):
     def __init__(self):
         super().__init__(debug=True)
-        self.interface.add_interface('jam.yaml',{'im_send':self.send})
+        self.interface.add_interface('jam.yaml',{'im_send':self.send,'im_send_to':self.send_to})
 
         # Setup socket for receiving messages
         self.purple_sock = self.context.socket(zmq.ROUTER)
@@ -22,7 +22,7 @@ class jam(m_node):
         self.purple = dbus.Interface(obj, "im.pidgin.purple.PurpleInterface")
 
         self.accounts = self.purple.PurpleAccountsGetAll()
-        self.buddies = {}
+        self.buddies = []
         for acc in self.accounts:
             bids = self.purple.PurpleFindBuddies(acc, "")
             
@@ -33,8 +33,8 @@ class jam(m_node):
                     "alias": self.purple.PurpleBuddyGetAlias(bid),
                     "account_id": acc
                 }
-                self.buddies[buddy["alias"]] = buddy
-        self.match_quality_threshold = 80
+                self.buddies.append(buddy)
+        self.match_quality_threshold = 95
         self.run()
 
     def purple_recv(self):
@@ -46,14 +46,27 @@ class jam(m_node):
         self.debug_print("PRPL DIED")
                         
     def send(self, header, args):
-        target,quality = process.extractOne(args['to'],self.buddies.keys())
-        if quality < self.match_quality_threshold:
-            self.debug_print("target={} not close enough to best match={}".format(target,args['to']))
+        candidates = process.extract({"name":args['to']},self.buddies,processor=lambda b: b['name'],limit=3)
+        candidates += process.extract({"alias":args['to']},self.buddies,processor=lambda b: b['alias'],limit=3)
+        candidates = sorted([c for c in candidates if c[1] > self.match_quality_threshold],key=lambda c: c[1])
+        self.debug_print("Filtered candidates:",candidates)
+        if len(candidates) == 1 or (len(candidates) == 2 and candidates[0][0] == candidates[1][0]):
+            buddy = candidates[0][0]
+        elif len(candidates) == 0:
+            self.debug_print("Target not found: {}".format(args['to']))
             return
-        buddy = self.buddies[target]
+        else:
+            self.debug_print("Ambiguous target: {}\nSome possible matches:\n{}".format(args['to'],"\n".join(["- Id: {}, Alias: {}, Quality: {}".format(c[0]['name'], c[0]['alias'], c[1]) for c in candidates])))
+            return
+        
         conv = self.purple.PurpleConversationNew(1, int(buddy['account_id']), str(buddy['name']))
         im = self.purple.PurpleConvIm(conv)
-        self.debug_print("SENDING",header,args,target,buddy,conv,im)
+        self.debug_print("SENDING",header,args,buddy,conv,im)
+        self.purple.PurpleConvImSend(im, args['msg'])
+
+    def send_to(self, header, args):
+        im = self.purple.PurpleConvIm(int(args['conv']))
+        self.debug_print("SENDING",header,args,im)
         self.purple.PurpleConvImSend(im, args['msg'])
 
         
